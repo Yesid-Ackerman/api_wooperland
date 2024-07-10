eebbd4880c8b9fa6253be42f4d29b1a38d93a420
Yesid-Ackerman <deironyesithym21@gmail.com>
2024-07-10 17:30:50 -0500

    CSS

--- resources/views/login/log.blade.php
+++ resources/views/login/log.blade.php
@@ -13,19 +13,58 @@
         .custom-pink {
             background-color: #EF476F;
         }
+        .stepper {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            margin-top: 20px;
+            padding: 0 10px;
+        }
+        .step {
+            background-color: #4A5568;
+            color: #FFFFFF;
+            padding: 10px;
+            border-radius: 9999px;
+            text-align: center;
+            width: 40px;
+        }
+        .step.active {
+            background-color: #EF476F;
+        }
+        .connector {
+            width: 30px;
+            height: 10px;
+            background-color: #f8fbff;
+        }
     </style>
 </head>
 
-<body class="bg-gray-100 h-screen flex items-center justify-center">
-
-    <div class="flex h-full w-full">
-        
-        <div class="flex-1 custom-bg flex flex-col items-start">
-            <img class="object-contain h-5/6 w-6/6 rounded-l-lg my-auto ml-0" src="{{ asset('img/image.png') }}" alt="">
-        </div>
-        <div class="flex-1 custom-pink flex flex-col justify-center items-center p-6 rounded-r-lg w-full -ml-64">
+<body class="bg-orange-500 h-screen flex items-center justify-center">
+    <div class="flex items-center justify-center h-screen w-full custom-bg">
+        <div class="flex items-center justify-center w-full max-w-screen-lg">
+            <div class="custom-bg flex justify-center items-center w-1/2 rounded-lg">
+                <img class="object-contain h-auto w-full rounded-lg" src="{{ asset('img/image.png') }}" alt="">
+            </div>
+            <div class="custom-pink flex flex-col justify-center items-center w-1/2 rounded-lg">
+                <div class="stepper">
+                    <div class="step active">1</div>
+                    <div class="connector"></div>
+                    <div class="step">2</div>
+                    <div class="connector"></div>
+                    <div class="step">3</div>
+                    <div class="connector"></div>
+                    <div class="step">4</div>
+                </div>
+                <div class="flex flex-col justify-center items-center gap-4 mt-4">
+                    <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-4/5">Botón 1</button>
+                    <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-4/5">Botón 2</button>
+                    <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-4/5">Botón 3</button>
+                    <button class="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded w-4/5">Botón 4</button>
+                </div>
+            </div>
         </div>
     </div>
+    
 </body>
 
 </html>
